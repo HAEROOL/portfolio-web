cd2eb6570bb1c2d4a9ebeb12039776faf6e7405a
haerool <gofn080776@gmail.com>
2026-01-20 23:14:28 +0900

    chore: 페이지 이름 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Haeroo Lee Portfolio',
-  description: 'Haeroo Lee Portfolio Page',
+  description: 'Haeroo Lee Portfolio',
   keywords: ['Haeroo Lee', 'Portfolio', 'frontend', 'developer', 'Haeroo Lee Frontend Developer'],
   authors: [{ name: 'Haeroo Lee' }],
   robots: {
